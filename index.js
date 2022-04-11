38ddfbb0fb969b4db3f925ee175835324e726379
Praveen Soreng <praveensoreng9@gmail.com>
2022-04-11 23:41:36 +0530

    added files

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("working");
+
+
+
+// const findProfitLoss = (CP,SP) =>{
+//     return SP>CP? "Profit":"Loss";
+// }
+// console.log(findProfitLoss(500,500));
+
+
+
+
+
+let number = 1900;
+console.log(number);
+console.log(number.toString().split('').reverse().join(''));
+// let str = number.toString();
+// let arr = str.split('');
+// let arr1 = arr.reverse();
+// console.log(arr1.join(''));
+
+// console.log(arr.push(5));
+
+
+// var Reverse_Number = (N) => {
+  
+  
+
+// };
+ 
